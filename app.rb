9354270ea6cac50d8d722e449b741181f0ed246d
Spac3Man2 <Spac3Man2>
2017-02-17 11:31:04 -0500

    updated favorite_num

--- app.rb
+++ app.rb
@@ -1,26 +1,29 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
-
+#enable :sessions
 get "/" do
 	erb :get_name 
 end
 
 post '/name' do
 	name = params[:user_name]
+	#session[:name] = params[:user_name]
 	erb :location, :locals => {:name => name}
 end
 
 post '/location' do
     location = params[:user_location]
+    #session[:location] = params[:user_location]
     name = params[:user_name]
     "#{name} your location is #{location}"
-    erb :get_age, :locals => {:name => name, :location => location}
+    erb :get_age , :locals => {:name => name, :location => location}
 
     #{}"Hello #{name}"
 end
 
 post '/age' do
 	age = params[:user_age]
+	#session[:age] = params[:user_age]
 	name = params[:user_name]
 	location = params[:user_location]
 	"#{name} your age is #{age}"
@@ -32,13 +35,17 @@ post '/favorite' do
 	favorite1 = params[:user_favorite_numbers1]
 	favorite2 = params[:user_favorite_numbers2]
 	favorite3 = params[:user_favorite_numbers3]
+	sum = favorite1.to_i + favorite2.to_i + favorite3.to_i
+    #session[:favorite1] = params[:user_favorite_numbers1]
+	#session[:favorite2] = params[:user_favorite_numbers2]
+	#session[:favorite3] = params[:user_favorite_numbers3]
     name = params[:user_name]
 	location = params[:user_location]
 	age = params[:user_age]
     erb :final_results, :locals => {:name => name, 
         :location => location, :age => age,
         :favorite1 => favorite1, :favorite2 => favorite2,
-        :favorite3 => favorite3}
+        :favorite3 => favorite3, :sum => sum}
 	
 end
 
@@ -53,15 +60,17 @@ end
 # end
 
 post '/final' do
-    final = params[:final_user_results]
+    final = params[:final_user_results]   
+    #seesioin[:final] = params[:final_user_results]
 	favorite1 = params[:user_favorite_numbers1]
 	favorite2 = params[:user_favorite_numbers2]
 	favorite3 = params[:user_favorite_numbers3]
-	#puts "#{params[:favorite1].to_i + params[:favorite2].to_i + params[:favorite3]}"
+	#sum = favorite1.to_i + favorite2.to_i + favorite3.to_i
     name = params[:user_name]
 	location = params[:user_location]
 	age = params[:user_age]
-	erb :final_results
+	sum = params[:user_sum]
+	erb :final_results #, :locals => {:sum => sum}
 end
 
 # sum = params[:user_favorite_numbers1].to_i + params[:user_favorite_numbers2].to_i + params[:user_favorite_numbers3]
